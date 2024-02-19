83c8b3db421b9c54ae53656ac4b9cc9bb9cc320e
DeeptenduKuri <deeptendukuri@gmail.com>
2024-02-19 19:32:56 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React , {useState} from "react";
+
+const customDictionary = {
+  teh: "the",
+  wrok: "work",
+  fot: "for",
+  exampl: "example"
+};
+
 
 function App() {
+  const [text,setText]=useState("")
+  const [suggestion,setSuggestion]=useState("")
+
+
+  const handleChange=(e)=>{
+    const text=e.target.value;
+    setText(text)
+    const words = text.split(" ")
+    const correctedWords=words.map((word)=>{
+      const correctedWord=customDictionary[word.toLowerCase()]
+      return correctedWord || word;
+    })
+    const correctedText=correctedWords.join(' ')
+    const firstCorrection=correctedWords.find((word,idx)=>word!==words[idx])
+    setSuggestion(firstCorrection)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Spell Check and Auto-Correction</h1>
+      <textarea value={text} placeholder="Enter text..." rows={5} columns={40} onChange={handleChange}/>
+      {suggestion && (<p>
+        Did you mean: <strong>{suggestion}</strong>
+      </p>)}
     </div>
   );
 }
